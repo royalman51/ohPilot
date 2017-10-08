d442d2e886632b53664d19611892fb3f2fec4609
royalman51 <royalman51@hotmail.com>
2017-10-08 12:01:05 +0200

    changed LED controller

--- LEDcontroller/LEDcontroller.ino
+++ LEDcontroller/LEDcontroller.ino
@@ -5,11 +5,11 @@ void setup() {
   // put your setup code here, to run once:
   //Serial.begin(115200);
   //initialize output pins
-  pinMode(A0,INPUT);
-  pinMode(A1,OUTPUT); //LED RightForward
-  pinMode(A2,OUTPUT); //LED RightAft
-  pinMode(A3,OUTPUT); //LED LeftAft
-  pinMode(A5,OUTPUT); //LED LeftForward
+  pinMode(A7,INPUT);
+  pinMode(7,OUTPUT); //LED RightForward
+  pinMode(8,OUTPUT); //LED RightAft
+  pinMode(9,OUTPUT); //LED LeftAft
+  pinMode(10,OUTPUT); //LED LeftForward
     
 }
 
@@ -36,23 +36,23 @@ void loop() {
 
 void toggleLED(int stat){
   if (stat == 1){
-    analogWrite(A1,255);
-    analogWrite(A2,255);
-    analogWrite(A3,255);
-    analogWrite(A5,255);
+    digitalWrite(7,HIGH);
+    digitalWrite(8,HIGH);
+    digitalWrite(9,HIGH);
+    digitalWrite(10,HIGH);
   }
   if (stat == 0){
-    analogWrite(A1,0);
-    analogWrite(A2,0);
-    analogWrite(A3,0);
-    analogWrite(A5,0);
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW);
+    digitalWrite(9,LOW);
+    digitalWrite(10,LOW);
   }
 
 }
 
 
 void getVbat(){
-  readV = readV *0.7 + 0.3 * analogRead(A0);  
+  readV = readV *0.7 + 0.3 * analogRead(A7);  
   Vbat = 0.012244897959184 * readV -0.085102040816353;
   Vbatavg = Vbatavg*0.95+Vbat*0.05;
 }
